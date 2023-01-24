41fd10b1a80ac3da7deeac1e4a66aa488d12aaf8
Danik-1 <lyatorovskiy@gmail.com>
2023-01-24 21:18:24 +0300

    Creating index.html and logic.js files

---
+++ logic.js
@@ -0,0 +1,131 @@
+//My javascript tutorial
+"use strict";
+
+console.log("hello", "world"); //console output
+
+//Variable scopes
+var num1 = 1; //global
+let num2 = 2; // local
+const num3 = 3; //local unchangable
+// num4 = 4 // could not be used in strict mode
+
+//Basic data types
+const number = 1;
+const bigInteger = 12345678901234567890n;
+const string = "string" + 'string' + `string ${number}`;
+const boolean = true;
+const array = [1, 2, 3, 4, 5];
+const object = { name: "name", age: 1 };
+function name(params) {
+    return params;
+}
+const nullValue = null;
+const undefinedValue = undefined;
+
+// JS is not strict typed language
+let variable = 1 / 0; // Infinity (number)
+variable = true;
+variable = "string" / 2; // NaN (number)
+console.log(typeof variable);
+
+//Math operators
+num2 += (num1 + num3);
+num2 -= (num1 - num3);
+num2 %= (num1 % num3);
+num2 *= (num1 * num3);
+num2 /= (num1 / num3);
+num1++;
+++num1;
+num2--;
+--num2;
+
+// Math functions
+Math.abs(-1); //absolute value
+Math.ceil(1.1); //round up
+Math.floor(1.9); //round down
+Math.max(1, 2, 3, 4, 5); //max value
+Math.min(1, 2, 3, 4, 5); //min value
+Math.pow(2, 3); //power
+Math.random() //random number
+Math.round(1.5); //round
+Math.sqrt(4); //square root
+
+// Logical operator
+let condition = (1 <= 2) && (3 >= 1);
+condition = (1 < 2) || (3 > 1);
+condition = !(1 < 2) ^ (3 > 1);
+condition = (1 == '1') && (1 != '2'); // with cast
+condition = (1 === '1') && (1 !== '2'); // without cast
+
+// Conditional operators
+const age = 30;
+if (18 < age < 65) {
+    console.log("You are old enough");
+} else if (age > 65) {
+    console.log("You are too old");
+} else {
+    console.log("You are too young");
+}
+
+const hungry = true;
+const action = hungry ? "eat" : "do not eat";
+
+const weather = "sunny";
+switch (weather) {
+    case "sunny":
+        console.log("wear sunglasses");
+        break;
+    case "rainy":
+        console.log("bring umbrella");
+        break;
+    default:
+        console.log("wear jacket");
+}
+
+// Loops
+let num = 5;
+while (num < 10) {
+    num++;
+}
+
+do {
+    num++;
+} while (num < 15);
+
+let j = 0;
+firstLoop: for (; j < 10; j += 2) {
+    secondLoop: while (true) {
+        for (let i = 0; i < 10; ++i) {
+            num++;
+
+            if (i == 5) {
+                continue;
+            }
+            if (i == 6) {
+                continue firstLoop;
+            }
+            if (i == 7) {
+                break;
+            }
+            if (i == 8) {
+                break secondLoop;
+            }
+        }
+    }
+}
+
+// Functions
+function printSum(a, b) {
+    console.log(a + b);
+}
+function getSum(a = 0, b = 0) {
+    return a + b;
+}
+
+const empty = function () { return; };
+
+const sum = (a, b) => a + b;
+const descriminant = (a, b, c) => {
+    return b * b - 4 * a * c;
+}
+console.log(sum);
