c86dd7bbf53f35d018937e1a0700a959ea8ab4d4
Danik-1 <lyatorovskiy@gmail.com>
2023-01-25 21:13:11 +0300

    Added strings

--- logic.js
+++ logic.js
@@ -67,6 +67,33 @@ Math.round(1.5); //round
 Math.sqrt(4); //square root
 num2.toFixed(3); // 3 digits after . // returns string
 
+// Strings
+let str1 = "hey";
+const str2 = 'hey';
+const str3 =
+`hey ${str1}
+ho ${str2}`;
+
+const str1Length = str1.length;
+const str1FirstCharacter = str1.charAt(0); // returns const
+const str1lastCharacter = str1[str1.length - 1];
+const substr1 = str1.slice(0, - 2); // start includes end doesn't
+const substr2 = str1.slice(1); // from 1 to end
+
+for (const char of str1) {
+    console.log(char);
+}
+
+str1 = str1.toUpperCase();
+str1 = str1.toLowerCase();
+const indexOf = str3.indexOf("h", 3); // (substr, start index) // -1 if not found
+const includes = str3.includes("h");
+const startsWith = str1.startsWith("a");
+const endsWith = str1.endsWith("a"); 
+
+
+
+
 // Logical operator
 let condition = (1 <= 2) && (3 >= 1);
 condition = (1 < 2) || (3 > 1);
